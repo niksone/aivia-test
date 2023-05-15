12b2a2bda9b6c5bf5aea29237ed4fc97b977abd6
Krekoten <nikita.krekoten@codex-soft.com>
2023-05-15 18:18:27 +0300

    refactor: remove excess code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
-import pinia from './plugins/pinia'
 import router from './router'
 
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,2 @@
-const HomePage = () => import('@/views/Home/HomePage.vue')
 
-export const routes = [
-  {
-    component: HomePage,
-    name: 'home',
-    path: '/',
-  },
-]
+export const routes = []
